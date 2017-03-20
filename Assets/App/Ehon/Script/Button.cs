513fb44a3c0dd18fdd9e23f8f91f4b2e81eb4c25
大島彩華 <1515honoka@gmail.com>
2017-03-20 19:06:59 +0900

    絵本アプリ作業中

---
+++ Assets/App/Ehon/Script/Button.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour {
+    //
+    private bool isNextButton = true;
+    //
+    private bool isBackButton = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+public void Get bool isNextButton()
+{
+    if (this.) ? {
+
+        this.isNextButton = true;
+    }
+}
+public void GetMyPageComtroller()
+{
+    this.isBackButton = false;
+}
