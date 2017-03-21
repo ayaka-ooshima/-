eed4db70bf8c3f290995378f3f066c80fafafe77
大島彩華 <1515honoka@gmail.com>
2017-03-21 13:40:20 +0900

    追加

--- Assets/App/Ehon/Script/Button.cs
+++ Assets/App/Ehon/Script/Button.cs
@@ -3,30 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Button : MonoBehaviour {
+public class Button : MonoBehaviour
+{
     //
     private bool isNextButton = true;
     //
     private bool isBackButton = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
-public void Get bool isNextButton()
-{
-    if (this.) ? {
+    // Use this for initialization
+    void Start()
+    {
 
-        this.isNextButton = true;
     }
-}
-public void GetMyPageComtroller()
-{
-    this.isBackButton = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void GetNextButton()
+    {
+        this.isNextButton = true;
+
+    }
+    public void GetMyPageComtroller()
+    {
+        this.isBackButton = false;
+    }
+
 }
