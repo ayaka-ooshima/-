f0ebcac7c499a77102e228d905118f8a992f23fb
大島彩華 <1515honoka@gmail.com>
2017-03-21 14:14:00 +0900

    3/21ページ切り替え

--- Assets/App/Ehon/Script/PageController.cs
+++ Assets/App/Ehon/Script/PageController.cs
@@ -1,16 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class PageController : MonoBehaviour {
+public class PageController : MonoBehaviour
+{
+    //2.ページをすべて管理する
+    [SerializeField]//インスペクタから設定するための記号
+    private List<Page> _pageList;
+    //現在のページ数
+    [SerializeField]
+    private int _currentPageNumber;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //1.ページを切り替える処理
+
+    /// <summary>
+    /// Start this instance.
+    /// </summary>
+    private void Start()
+    {
+        _currentPageNumber = 1;
+        //ヒエラルキー階層の一番下い来る
+        _pageList[0].transform.SetAsLastSibling();
+    }
+
+    //====ボタンのイベントを受け取る===//
+
+    /// <summary>
+    /// Nextボタンが押されたときに呼ばれる 
+    /// </summary>
+    public void OnNextButtonTap()
+    {
+        //ページ数を増やす
+        _currentPageNumber = Math.Min(_pageList.Count - 1, _currentPageNumber + 1);
+        //ヒエラルキー階層の一番下に来る
+        _pageList[_currentPageNumber].transform.SetAsLastSibling();
+    }
+
+    /// <summary>
+    /// Backボタンが押されたときに呼ばれる 
+    /// </summary>
+    public void OnBackButtonTap()
+    {
+        //ページ数を増やす
+        _currentPageNumber = Math.Max(0, _currentPageNumber - 1);
+        //ヒエラルキー階層の一番下に来る
+        _pageList[_currentPageNumber].transform.SetAsLastSibling();
+    }
 }
