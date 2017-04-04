cd5fdc15a3bfa30ab070efdbfd2eb268804b10ea
大島彩華 <1515honoka@gmail.com>
2017-04-04 14:17:35 +0900

    エラー修正

---
+++ Assets/App/Ehon/Script/ImageFadeInEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ImageFadeInEvent : EventBase
+{
+    [SerializeField]
+    private Image _image;
+    [SerializeField]
+    private float _fadeTime;
+
+    /// <summary>
+    /// 初期化の関数
+    /// </summary>
+    public override void Initialize()
+    {
+        _image.DOFade(0f, 0f);
+    }
+
+    /// <summary>
+    /// イベントが実行されるときに呼ばれる
+    /// </summary>
+    public override void ExecuteAction()
+    {
+        _image.DOFade(1f, _fadeTime);
+    }
+}
