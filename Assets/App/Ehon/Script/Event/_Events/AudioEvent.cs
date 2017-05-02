cbb8eb6045c0ab839b2ad721eca157c2cc68d058
大島彩華 <1515honoka@gmail.com>
2017-05-02 21:25:55 +0900

    ＢＧＭ、テキスト作業中

---
+++ Assets/App/Ehon/Script/Event/_Events/AudioEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioEvent : EventBase
+{
+    [SerializeField]
+    private AudioSource _audioSource;
+
+    /// <summary>
+    /// 初期化の関数
+    /// </summary>
+    public override void Initialize()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// Raises the execute event.
+    /// </summary>
+    protected override void OnExecute()
+    {
+        _audioSource.Play();
+    }
+
+    /// <summary>
+    /// Raises the complete event.
+    /// </summary>
+    protected override void OnComplete()
+    {
+        _audioSource.Stop();
+    }
+}
